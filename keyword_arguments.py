1d65d74365babeede8e558774590cd1c5a2a29e2
NoRhythmDEV <138884236+NoRhythmDEV@users.noreply.github.com>
2023-11-23 16:24:00 +0100

    keywords

---
+++ keyword_arguments.py
@@ -0,0 +1,8 @@
+#keyword arguments = args preceded by and identifier when we pass them to a function
+# the order doesnt matter unlike positional args
+
+
+def hello(last,middle,first):
+    print("Hello "+first+" "+middle+" "+last)
+
+hello(first="first",middle="middle",last="last")
