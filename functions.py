cf8aa7d2d4c013a87fe2f857ad3fcffe89cf3c16
NoRhythmDEV <138884236+NoRhythmDEV@users.noreply.github.com>
2023-11-23 15:41:47 +0100

    AHHHHHHHHHH

--- functions.py
+++ functions.py
@@ -1,9 +1,9 @@
 #functions = a block of code that will only be called when its called
 
+#takes in arguments
+def hello(firstname,lastname,age):
+    print("Hello " + firstname + " " + lastname + ", you are " + str(age) + " years old")
 
-def hello(firstname,lastname):
-    print("Hello " + firstname + " " + lastname)
-
-
-hello("Max","Mustermann")
+#send arguments to functions
+hello("Max","Mustermann",21)
  
