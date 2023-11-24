ab429430655416d1847b10bac855d7850cd8ab54
NoRhythmDEV <138884236+NoRhythmDEV@users.noreply.github.com>
2023-11-24 10:43:37 +0100

    added indexing

--- string_format.py
+++ string_format.py
@@ -8,3 +8,5 @@ item = "moon"
 print("The "+animal+" jumped over the "+ item)
 #method 2 with formation function and given values {}=placeholder (format fields), first format field will fill in the first value, then the next and so on
 print("The {} jumped over the {}".format(animal,item))
+#method 3 same as 2 but with indexing    i=0   i=1 ....
+print("The {1} jumped over the {0}".format(item,animal))
