01a44275ab2ffd62f44cae10c2063559c8b6f575
NoRhythmDEV <138884236+NoRhythmDEV@users.noreply.github.com>
2023-11-29 16:34:13 +0100

    youtube downloader

--- Mini Projects/YT-Downloader/main.py
+++ Mini Projects/YT-Downloader/main.py
@@ -8,16 +8,30 @@ customtkinter.set_default_color_theme("blue")
 
 #Functions
 
-def startDownload():
+def hideErrorMessage():
+    errorMessage.configure(text="")
+
+def startDownload_highres():
     try:
-        ytLink = link.get
+        ytLink = link.get()
         ytObject = YouTube(ytLink)
         video_highRes = ytObject.streams.get_highest_resolution()
-        video_highRes.download
+        video_highRes.download()
+        finishLabel.configure(text="Download Complete")
     except:
-        print("Youtube link is invalid")
-    print("Download Complete")
+        errorMessage.configure(text="Invalid Link")
+        app.after(5000, hideErrorMessage)
 
+def startDownload_audioOnly():
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink)
+        audioOnly = ytObject.streams.get_audio_only()
+        audioOnly.download()
+        finishLabel.configure(text="Download Complete")
+    except:
+        errorMessage.configure(text="Invalid Link")
+        app.after(5000, hideErrorMessage)
 
 # App Frame
 
@@ -25,7 +39,7 @@ app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("YouTube Downloader")
 
-# UI Elements
+# ------------------------------------------------- UI Elements -------------------------------------------------
 
 # Title
 title = customtkinter.CTkLabel(app, text="Insert YouTube Link")
@@ -36,15 +50,21 @@ url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
 
-#Button
-download_button = customtkinter.CTkButton(app, text="Download!", command=startDownload)
-download_button.pack(padx=10,pady=10)
+# Button high_res
+download_button_highRes = customtkinter.CTkButton(app, text="Download Mp4 (Highest Res)", command=startDownload_highres,width=200,height=40)
+download_button_highRes.pack(padx=10,pady=10)
 
+# Button audioOnly
+download_button_audioOnly = customtkinter.CTkButton(app, text="Download Mp3", command=startDownload_audioOnly,width=200,height=40)
+download_button_audioOnly.pack(padx=10, pady=10)
 
+# Finished Downloading
+finishLabel = customtkinter.CTkLabel(app,text="",text_color="green",font=("Helvatica",25))
+finishLabel.pack()
 
-
-
-
+# Error Message if link Invalid
+errorMessage = customtkinter.CTkLabel(app, text="", text_color="red",font=("Helvatica",25))
+errorMessage.pack()
 
 
 
