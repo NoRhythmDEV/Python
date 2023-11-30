55efc7060f8692f817eda0f87f3ae679bf4c8571
NoRhythmDEV <138884236+NoRhythmDEV@users.noreply.github.com>
2023-11-30 16:25:38 +0100

    Title Display, Progress Bar, Thumbnail Preview

--- Mini Projects/YT-Downloader/main.py
+++ Mini Projects/YT-Downloader/main.py
@@ -1,49 +1,74 @@
 import tkinter
 import customtkinter
 from pytube import YouTube
+from PIL import Image, ImageTk, ImageFilter
+from urllib.request import urlopen
+import requests
+
+# Functions
+
+def hideErrorMessage():
+    errorMessage.configure(text="")
+
+def hideSuccessMessage():
+    successMessage.configure(text="")
+
+def hideThumbail():
+    thumbnailDisplay.configure(image=placeholder_image, width=400, height=225)
+
+def hideTitle():
+    titleDisplay.configure(text="")
+
+def startDownload():
+    global url
+    try:
+        ytLink = link.get()
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
+
+        url = str(ytObject.thumbnail_url)
+        response = requests.get(url, stream=True)
+        image = Image.open(response.raw)
+        image = image.resize((400, 225), Image.LANCZOS)  # Resize the image
+        thumbnail_image = ImageTk.PhotoImage(image)
+
+        titleDisplay.configure(text=f"Downloading: {ytObject.title}")
+        thumbnailDisplay.configure(image=thumbnail_image, width=400, height=225)
+
+        video_highRes = ytObject.streams.get_highest_resolution()
+        video_highRes.download()
+
+        successMessage.configure(text="Download Complete")
+
+        # Remove Titles and Displays
+        app.after(5000, hideSuccessMessage)
+        app.after(5000, hideThumbail)
+        app.after(5000, hideTitle)
+    except Exception as e:
+        print(e)
+        errorMessage.configure(text="Invalid Link")
+        app.after(5000, hideErrorMessage)
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = (bytes_downloaded / total_size) * 100
+    per = str(int(percentage_of_completion))
+    pPercantage.configure(text=f"{per}%")
+    pPercantage.update()
+
+    #update pBar
+    pBar.set(float(percentage_of_completion)/100)
 
 # System Settings
 customtkinter.set_appearance_mode("System") #uses system settings (light/dark mode)
 customtkinter.set_default_color_theme("blue")
 
-#Functions
-
-def hideErrorMessage():
-    errorMessage.configure(text="")
-
-def startDownload_highres():
-    try:
-        ytLink = link.get()
-        ytObject = YouTube(ytLink)
-        video_highRes = ytObject.streams.get_highest_resolution()
-        video_highRes.download() 
-        finishLabel.configure(text="Download Complete")
-    except:
-        errorMessage.configure(text="Invalid Link")
-        app.after(5000, hideErrorMessage)
-
-def startDownload_audioOnly():
-    try:
-        ytLink = link.get()
-        ytObject = YouTube(ytLink)
-        audioOnly = ytObject.streams.get_audio_only()
-        audioOnly.download()
-        finishLabel.configure(text="Download Complete")
-    except:
-        errorMessage.configure(text="Invalid Link")
-        app.after(5000, hideErrorMessage)
-
 # App Frame
-
 app = customtkinter.CTk()
-app.geometry("720x480")
+app.geometry("1280x720")
 app.title("YouTube Downloader")
 
 # ------------------------------------------------- UI Elements -------------------------------------------------
-
-# Title
-title = customtkinter.CTkLabel(app, text="Video: ")
-title.pack()
 
 # Link Input
 url_var = tkinter.StringVar()
@@ -51,45 +76,33 @@ link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
 
 # Button high_res
-download_button_highRes = customtkinter.CTkButton(app, text="Download Mp4 (Highest Res)", command=startDownload_highres,width=200,height=40)
+download_button_highRes = customtkinter.CTkButton(app, text="Download Video", command=startDownload,width=200,height=40)
 download_button_highRes.pack(padx=10,pady=10)
 
-# Button audioOnly
-download_button_audioOnly = customtkinter.CTkButton(app, text="Download Mp3", command=startDownload_audioOnly,width=200,height=40)
-download_button_audioOnly.pack(padx=10, pady=10)
+# Progress
+pPercantage = customtkinter.CTkLabel(app, text="0%")
+pPercantage.pack()
+
+pBar = customtkinter.CTkProgressBar(app, width=400)
+pBar.set(0)
+pBar.pack()
+
+# Title Display
+titleDisplay = customtkinter.CTkLabel(app, text="", font=("Helvetica",18))
+titleDisplay.pack()
+
+# Thumbnail Display
+placeholder_image = ImageTk.PhotoImage(Image.new("RGB", (0, 0), "white"))
+thumbnailDisplay = customtkinter.CTkLabel(app, text="", image=placeholder_image, width=400, height=225)
+thumbnailDisplay.pack()
 
 # Finished Downloading
-finishLabel = customtkinter.CTkLabel(app,text="",text_color="green",font=("Helvatica",25))
-finishLabel.pack()
+successMessage = customtkinter.CTkLabel(app,text="",text_color="green",font=("Helvetica",25))
+successMessage.pack()
 
 # Error Message if link Invalid
-errorMessage = customtkinter.CTkLabel(app, text="", text_color="red",font=("Helvatica",25))
+errorMessage = customtkinter.CTkLabel(app, text="", text_color="red",font=("Helvetica",25))
 errorMessage.pack()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # Run App
-
 app.mainloop()
