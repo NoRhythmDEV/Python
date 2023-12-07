2b2d529f1e9856c184a374c9a33cebecf69ce513
NoRhythmDEV <138884236+NoRhythmDEV@users.noreply.github.com>
2023-12-07 16:02:51 +0100

    added filedialog

--- Mini Projects/PDF Tool/main.py
+++ Mini Projects/PDF Tool/main.py
@@ -12,29 +12,32 @@ def merge_pdf():
     if not input_path:
         return
 
+    pdf_files = [file for file in os.listdir(input_path) if file.endswith(".pdf")]
+
+    if not pdf_files:
+        messagebox.showerror("ERROR", f"No .pdf files found in: {input_path}")
+        return
+
     output_path = os.path.join(input_path, "output")
-    os.makedirs(output_path, exist_ok=1)
+    os.makedirs(output_path, exist_ok=True)
     files_to_remove = []
 
     merger = PdfMerger()
 
-    for file in os.listdir(input_path):
-        if file.endswith(".pdf"):
-            file_path = os.path.join(input_path, file)
-            print(file_path)
-            merger.append(file_path)
-            files_to_remove.append(file_path)
-            messagebox.showinfo("Success", f"Done! Files output: {output_path}")
-        else:
-            messagebox.showerror("ERROR", f"No .pdf Files found in: {input_path}")
+    for file in pdf_files:
+        file_path = os.path.join(input_path, file)
+        print(file_path)
+        merger.append(file_path)
+        files_to_remove.append(file_path)
+
     output_file_path = os.path.join(output_path, "merged.pdf")
+    messagebox.showinfo("Success", f"Done! Files output: {output_path}")
     merger.write(output_file_path)
     merger.close()
 
-
-    # Remove the original files
-    for file_to_remove in files_to_remove:
-        os.remove(file_to_remove)
+    for i in files_to_remove:
+        os.remove(i)
+        
 # Main Window
 
 app = tkb.Window(themename="darkly")
